80743dbc2391ea766c934e03a6d68b86271d24ff
steokelly <steokelly@users.noreply.github.com>
2026-08-19 22:46:26 +0100

    force fresh data on every load

--- app/episodes/page.js
+++ app/episodes/page.js
@@ -1,6 +1,7 @@
 import { getSupabaseServerClient } from '../../lib/supabase';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0; export const fetchCache = 'force-no-store';
 
 export default async function EpisodesPage() {
   const supabase = getSupabaseServerClient();
